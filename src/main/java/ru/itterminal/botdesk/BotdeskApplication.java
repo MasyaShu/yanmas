9cdcc7c54ef399bbf72bd0570066b817ed00f7ff
yaneg-ru <yaneg.ru@gmail.com>
2020-08-31 11:18:09 +0500

    initial

---
+++ src/main/java/ru/itterminal/botdesk/BotdeskApplication.java
@@ -0,0 +1,13 @@
+package ru.itterminal.botdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotdeskApplication.class, args);
+	}
+
+}
