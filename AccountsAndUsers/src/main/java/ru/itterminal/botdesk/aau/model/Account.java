499e12373386db2180d640c171a26b64acc32eb1
yaneg-ru <yaneg.ru@gmail.com>
2020-09-01 11:04:24 +0500

    add Account

--- AccountsAndUsers/src/main/java/ru/itterminal/botdesk/aau/model/Account.java
+++ AccountsAndUsers/src/main/java/ru/itterminal/botdesk/aau/model/Account.java
@@ -1,4 +1,53 @@
 package ru.itterminal.botdesk.aau.model;
 
-public class Account {
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "account")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account extends BaseEntity {
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String language;
+
+    @Column(nullable = false)
+    private String time_zone;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Account)) {
+            return false;
+        }
+        Account account = (Account) o;
+        return Objects.equals(name, account.name) &&
+                Objects.equals(language, account.language) &&
+                Objects.equals(time_zone, account.time_zone) &&
+                Objects.equals(getId(), account.getId()) &&
+                Objects.equals(getVersion(), account.getVersion()) &&
+                Objects.equals(getDeleted(), account.getDeleted());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, language, time_zone);
+    }
 }
