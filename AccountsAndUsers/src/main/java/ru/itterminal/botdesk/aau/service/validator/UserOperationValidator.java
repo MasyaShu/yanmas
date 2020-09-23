0fb43357a4092b70a2e3835e17f30614a923ad78
yaneg-ru <yaneg.ru@gmail.com>
2020-09-23 10:16:32 +0500

    delete repository (replace service)

--- AccountsAndUsers/src/main/java/ru/itterminal/botdesk/aau/service/validator/UserOperationValidator.java
+++ AccountsAndUsers/src/main/java/ru/itterminal/botdesk/aau/service/validator/UserOperationValidator.java
@@ -1,27 +1,27 @@
 package ru.itterminal.botdesk.aau.service.validator;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Component;
-import ru.itterminal.botdesk.aau.model.User;
-import ru.itterminal.botdesk.aau.model.projection.UserUniqueFields;
-import ru.itterminal.botdesk.aau.repository.UserRepository;
-import ru.itterminal.botdesk.aau.service.impl.UserServiceImpl;
-import ru.itterminal.botdesk.commons.exception.EntityNotExistException;
-import ru.itterminal.botdesk.commons.exception.LogicalValidationException;
-import ru.itterminal.botdesk.commons.exception.error.ValidationError;
-import ru.itterminal.botdesk.commons.service.validator.impl.BasicOperationValidatorImpl;
+import static java.lang.String.format;
+import static java.util.Collections.singletonList;
+import static ru.itterminal.botdesk.commons.service.CrudService.ENTITY_NOT_EXIST_MESSAGE;
+import static ru.itterminal.botdesk.commons.service.CrudService.UPDATE_INIT_MESSAGE;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-import static java.lang.String.format;
-import static java.util.Collections.singletonList;
-import static ru.itterminal.botdesk.commons.service.CrudService.ENTITY_NOT_EXIST_MESSAGE;
-import static ru.itterminal.botdesk.commons.service.CrudService.UPDATE_INIT_MESSAGE;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+import ru.itterminal.botdesk.aau.model.User;
+import ru.itterminal.botdesk.aau.model.projection.UserUniqueFields;
+import ru.itterminal.botdesk.aau.service.impl.UserServiceImpl;
+import ru.itterminal.botdesk.commons.exception.EntityNotExistException;
+import ru.itterminal.botdesk.commons.exception.LogicalValidationException;
+import ru.itterminal.botdesk.commons.exception.error.ValidationError;
+import ru.itterminal.botdesk.commons.service.validator.impl.BasicOperationValidatorImpl;
 
 @Slf4j
 @Component
@@ -32,9 +32,6 @@ public class UserOperationValidator extends BasicOperationValidatorImpl<User> {
     @Autowired
     BCryptPasswordEncoder encoder;
 
-    @Autowired
-    UserRepository repository;
-
     @Override
     public boolean checkUniqueness(User entity) {
         log.trace(CHECK_UNIQUENESS, entity);
@@ -44,12 +41,12 @@ public class UserOperationValidator extends BasicOperationValidatorImpl<User> {
             log.trace(FIELDS_UNIQUE, entity);
             return true;
         } else {
-                String validatedField;
-                if (entity.getEmail().equalsIgnoreCase(foundUser.get(0).getEmail())) {
-                    validatedField = "email";
-                    errors.put(validatedField, singletonList(new ValidationError(NOT_UNIQUE_CODE,
-                            format(NOT_UNIQUE_MESSAGE, validatedField))));
-                }
+            String validatedField;
+            if (entity.getEmail().equalsIgnoreCase(foundUser.get(0).getEmail())) {
+                validatedField = "email";
+                errors.put(validatedField, singletonList(new ValidationError(NOT_UNIQUE_CODE,
+                        format(NOT_UNIQUE_MESSAGE, validatedField))));
+            }
             log.error(FIELDS_NOT_UNIQUE, errors);
             throw new LogicalValidationException(VALIDATION_FAILED, errors);
         }
@@ -59,7 +56,7 @@ public class UserOperationValidator extends BasicOperationValidatorImpl<User> {
     public boolean beforeCreate(User entity) {
         super.beforeCreate(entity);
         entity.setPassword(encoder.encode(entity.getPassword()));
-        if (entity.getLanguage()==null) {
+        if (entity.getLanguage() == null) {
             String ln = entity.getAccount().getLanguage();
             entity.setLanguage(ln);
         }
@@ -70,11 +67,12 @@ public class UserOperationValidator extends BasicOperationValidatorImpl<User> {
     public boolean beforeUpdate(User entity) {
         super.beforeUpdate(entity);
         log.trace(format(UPDATE_INIT_MESSAGE, entity.getClass().getSimpleName(), entity.getId(), entity));
-        User entityFromDatabase = repository.findById(entity.getId()).orElseThrow(() -> {
+        User entityFromDatabase = service.findById(entity.getId());
+        if (entityFromDatabase == null) {
             String message = format(ENTITY_NOT_EXIST_MESSAGE, entity.getClass().getSimpleName(), entity.getId());
             log.error(message);
-            return new EntityNotExistException(message);
-        });
+            throw new EntityNotExistException(message);
+        }
         if (!entity.getPassword().isEmpty()) {
             entity.setPassword(encoder.encode(entity.getPassword()));
         } else {
