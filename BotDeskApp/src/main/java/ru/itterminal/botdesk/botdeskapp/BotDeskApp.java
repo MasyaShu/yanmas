b7768b2f86930c76bd058146b8f8a3232aa08ad8
yaneg-ru <yaneg.ru@gmail.com>
2020-09-30 07:10:35 +0500

    make multiple module project add new module BotDeskApp

---
+++ BotDeskApp/src/main/java/ru/itterminal/botdesk/botdeskapp/BotDeskApp.java
@@ -0,0 +1,47 @@
+package ru.itterminal.botdesk.botdeskapp;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.instrument.classloading.InstrumentationLoadTimeWeaver;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import ru.itterminal.botdesk.commons.repository.ParentEntityRepositoryImpl;
+
+@SpringBootApplication(scanBasePackages = "ru.itterminal.botdesk")
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "ru.itterminal.botdesk", repositoryBaseClass = ParentEntityRepositoryImpl.class)
+public class BotDeskApp {
+
+    @Autowired
+    DataSource dataSource;
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(BotDeskApp.class, args);
+        for (String s : applicationContext.getBeanDefinitionNames()) {
+            System.out.println(s);
+        }
+    }
+
+    @Bean
+    public EntityManagerFactory entityManagerFactory() {
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(Boolean.FALSE);
+        vendorAdapter.setShowSql(Boolean.TRUE);
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("ru.itterminal.botdesk");
+        factory.setDataSource(dataSource);
+        factory.afterPropertiesSet();
+        factory.setLoadTimeWeaver(new InstrumentationLoadTimeWeaver());
+        return factory.getObject();
+    }
+}
