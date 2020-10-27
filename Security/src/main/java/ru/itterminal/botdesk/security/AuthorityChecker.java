46c7a3ea4a02e6f99410c367debe56c055bc11aa
yaneg-ru <yaneg.ru@gmail.com>
2020-10-27 11:01:07 +0500

    implement AuthorityChecker

---
+++ Security/src/main/java/ru/itterminal/botdesk/security/AuthorityChecker.java
@@ -0,0 +1,16 @@
+package ru.itterminal.botdesk.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import ru.itterminal.botdesk.jwt.JwtUser;
+
+@Component("authorityChecker")
+public class AuthorityChecker {
+
+    public boolean is_inner_group(Authentication authentication) {
+        JwtUser jwtUser = (JwtUser) authentication.getPrincipal();
+        return jwtUser.is_inner_group();
+    }
+
+}
