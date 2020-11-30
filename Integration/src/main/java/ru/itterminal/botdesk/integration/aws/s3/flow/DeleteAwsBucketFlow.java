88fa64f3a50ac261b52e4a6f147d3d6ac587c9a3
yaneg-ru <yaneg.ru@gmail.com>
2020-11-30 10:54:58 +0500

    implement DeleteAwsBucketFlow and cover one by unit tests

---
+++ Integration/src/main/java/ru/itterminal/botdesk/integration/aws/s3/flow/DeleteAwsBucketFlow.java
@@ -0,0 +1,40 @@
+package ru.itterminal.botdesk.integration.aws.s3.flow;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.Gateway;
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.annotation.Poller;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.QueueChannel;
+import org.springframework.messaging.MessageChannel;
+
+import ru.itterminal.botdesk.integration.aws.s3.S3BucketOperations;
+
+@Configuration
+public class DeleteAwsBucketFlow {
+
+    private final S3BucketOperations s3BucketOperations;
+
+    public DeleteAwsBucketFlow(S3BucketOperations s3BucketOperations) {
+        this.s3BucketOperations = s3BucketOperations;
+    }
+
+    @Bean
+    public MessageChannel deleteAwsBucketChannel() {
+        return new QueueChannel(100);
+    }
+
+    @MessagingGateway
+    public interface DeleteAwsBucketGateway {
+        @Gateway(requestChannel = "deleteAwsBucketChannel")
+        void process(String bucketName);
+    }
+
+    @ServiceActivator(inputChannel = "deleteAwsBucketChannel",
+            poller = @Poller(maxMessagesPerPoll = "1", fixedRate = "1000"))
+    private void deleteAwsBucket(String bucketName) {
+        s3BucketOperations.deleteBucket(bucketName);
+    }
+
+}
