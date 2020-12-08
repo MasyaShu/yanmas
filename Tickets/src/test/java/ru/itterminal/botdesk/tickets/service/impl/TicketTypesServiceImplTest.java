1bfb4d94ff8ea97ca9a07c66aefe87c5afc75f45
mShu <masyashu@gmail.com>
2020-12-08 16:03:42 +0500

    add TicketTypesServiceImplTest and TicketTypesOperationValidatorTest

---
+++ Tickets/src/test/java/ru/itterminal/botdesk/tickets/service/impl/TicketTypesServiceImplTest.java
@@ -0,0 +1,78 @@
+package ru.itterminal.botdesk.tickets.service.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import ru.itterminal.botdesk.aau.model.Account;
+import ru.itterminal.botdesk.aau.model.User;
+import ru.itterminal.botdesk.aau.service.validator.UserOperationValidator;
+import ru.itterminal.botdesk.commons.exception.EntityNotExistException;
+import ru.itterminal.botdesk.security.config.TestSecurityConfig;
+import ru.itterminal.botdesk.tickets.model.TicketTypes;
+import ru.itterminal.botdesk.tickets.repository.TicketTypesRepository;
+import ru.itterminal.botdesk.tickets.service.validator.TicketTypesOperationValidator;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SpringJUnitConfig(value = {TicketTypesServiceImpl.class})
+class TicketTypesServiceImplTest {
+
+    @MockBean
+    private TicketTypesRepository ticketTypesRepository;
+
+    @MockBean
+    private TicketTypesOperationValidator validator;
+
+    @Autowired
+    private TicketTypesServiceImpl service;
+
+    private TicketTypes ticketTypes;
+    private Account account;
+
+    @BeforeEach
+    void setUpBeforeEach() {
+        account = new Account();
+        account.setId(UUID.fromString(TestSecurityConfig.ACCOUNT_1_ID));
+        ticketTypes = TicketTypes
+                .builder()
+                .account(account)
+                .build();
+    }
+
+    @Test
+    void findByUniqueFields_shouldGetEntityNotExistException_whenUserIsNull() {
+        assertThrows(EntityNotExistException.class, () -> service.findByUniqueFields(null));
+        verify(ticketTypesRepository, times(0)).getByNameAndAccount_IdAndIdNot(any(), any(), any());
+    }
+
+    @Test
+    void findByUniqueFields_shouldGetEntityNotExistException_whenUserEmailIsNull() {
+        ticketTypes.setName(null);
+        assertThrows(EntityNotExistException.class, () -> service.findByUniqueFields(ticketTypes));
+        verify(ticketTypesRepository, times(0)).getByNameAndAccount_IdAndIdNot(any(), any(), any());
+    }
+
+    @Test
+    void findByUniqueFields_shouldGetEntityNotExistException_whenUserIdIsNull() {
+        ticketTypes.setId(null);
+        assertThrows(EntityNotExistException.class, () -> service.findByUniqueFields(ticketTypes));
+        verify(ticketTypesRepository, times(0)).getByNameAndAccount_IdAndIdNot(any(), any(), any());
+    }
+
+    @Test
+    void findByUniqueFields_shouldGetEntityNotExistException_whenUserAccountIsNull() {
+        ticketTypes.setAccount(null);
+        assertThrows(EntityNotExistException.class, () -> service.findByUniqueFields(ticketTypes));
+        verify(ticketTypesRepository, times(0)).getByNameAndAccount_IdAndIdNot(any(), any(), any());
+    }
+
+}
---
+++ Tickets/src/test/java/ru/itterminal/botdesk/tickets/service/validator/TicketTypesOperationValidatorTest.java
@@ -0,0 +1,71 @@
+package ru.itterminal.botdesk.tickets.service.validator;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import ru.itterminal.botdesk.aau.model.Account;
+import ru.itterminal.botdesk.commons.exception.LogicalValidationException;
+import ru.itterminal.botdesk.commons.exception.error.ValidationError;
+import ru.itterminal.botdesk.tickets.model.TicketTypes;
+import ru.itterminal.botdesk.tickets.model.projection.TicketTypesUniqueFields;
+import ru.itterminal.botdesk.tickets.service.impl.TicketTypesServiceImpl;
+
+import java.util.*;
+
+import static java.util.Collections.singletonList;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@SpringJUnitConfig(value = {TicketTypesOperationValidator.class})
+@ActiveProfiles("Test")
+class TicketTypesOperationValidatorTest {
+
+    @MockBean
+    private TicketTypesServiceImpl service;
+
+    @Mock
+    private TicketTypesUniqueFields ticketTypesUniqueFields;
+
+    @Autowired
+    private final TicketTypesOperationValidator validator = new TicketTypesOperationValidator(service);
+
+    private static final String EXIST_NAME = "ticketTypes1";
+    private static final Map<String, List<ValidationError>> errors = new HashMap<>();
+    private static LogicalValidationException logicalValidationException;
+    private static TicketTypes ticketTypes;
+
+    @BeforeAll
+    static void setUp() {
+        Account account1 = new Account();
+        account1.setId(UUID.fromString("bcf98101-2a22-42bf-94cc-c900b50a0b69"));
+        ticketTypes = TicketTypes
+                .builder()
+                .name(EXIST_NAME)
+                .account(account1)
+                .build();
+    }
+
+
+    @Test
+    void checkUniqueness_shouldGetTrue_whenPassedDataIsUnique() {
+        when(service.findByUniqueFields(any())).thenReturn(Collections.emptyList());
+        assertTrue(validator.checkUniqueness(new TicketTypes()));
+    }
+
+    @Test
+    void checkUniqueness_shouldGetLogicalValidationException_whenPassedDataNotUnique() {
+        when(service.findByUniqueFields(any())).thenReturn(List.of(ticketTypesUniqueFields));
+        when(ticketTypesUniqueFields.getName()).thenReturn(EXIST_NAME);
+        errors.put("name", singletonList(new ValidationError("not unique", "name is occupied")));
+        logicalValidationException = new LogicalValidationException("Validation failed", errors);
+        LogicalValidationException thrown = assertThrows(LogicalValidationException.class,
+                () -> validator.checkUniqueness(ticketTypes));
+        assertEquals(logicalValidationException.getFieldErrors().get("name").get(0),
+                thrown.getFieldErrors().get("name").get(0));
+    }
+}
