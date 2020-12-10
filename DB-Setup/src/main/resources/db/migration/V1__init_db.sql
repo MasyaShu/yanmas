3dfa3392fac60af9565ae33047fef0bc016b2ff0
yaneg-ru <yaneg.ru@gmail.com>
2020-12-10 09:52:05 +0500

    edit V1__init_db.sql for ticket_settings

--- DB-Setup/src/main/resources/db/migration/V1__init_db.sql
+++ DB-Setup/src/main/resources/db/migration/V1__init_db.sql
@@ -85,7 +85,6 @@ create TABLE IF NOT EXISTS ticket_types
     version       int4         NOT NULL DEFAULT (0),
     id            uuid         NOT NULL,
     name          varchar(128) NOT NULL,
-    comment       text,
     is_predefined bool         NOT NULL DEFAULT 'false',
     account_id    uuid         NOT NULL,
     PRIMARY KEY (id),
@@ -107,4 +106,50 @@ create TABLE IF NOT EXISTS ticket_statuses
     account_id             uuid         NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (account_id) REFERENCES account (id)
+);
+create TABLE IF NOT EXISTS ticket_settings
+(
+    out_id                      varchar(128),
+    deleted                     bool NOT NULL DEFAULT 'false',
+    version                     int4 NOT NULL DEFAULT (0),
+    id                          uuid NOT NULL,
+    account_id                  uuid NOT NULL,
+    author_id                   uuid,
+    ticket_type_id_for_new      uuid,
+    ticket_status_id_for_new    uuid,
+    ticket_status_id_for_reopen uuid,
+    ticket_status_id_for_close  uuid,
+    ticket_status_id_for_cancel uuid,
+    PRIMARY KEY (id),
+    FOREIGN KEY (account_id) REFERENCES account (id),
+    FOREIGN KEY (author_id) REFERENCES users (id),
+    FOREIGN KEY (ticket_type_id_for_new) REFERENCES ticket_types (id),
+    FOREIGN KEY (ticket_status_id_for_new) REFERENCES ticket_statuses (id),
+    FOREIGN KEY (ticket_status_id_for_reopen) REFERENCES ticket_statuses (id),
+    FOREIGN KEY (ticket_status_id_for_close) REFERENCES ticket_statuses (id),
+    FOREIGN KEY (ticket_status_id_for_cancel) REFERENCES ticket_statuses (id)
+);
+create TABLE IF NOT EXISTS ticket_settings_executors
+(
+    out_id             varchar(128),
+    deleted            bool NOT NULL DEFAULT 'false',
+    version            int4 NOT NULL DEFAULT (0),
+    id                 uuid NOT NULL,
+    ticket_settings_id uuid NOT NULL,
+    executor_id        uuid NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (ticket_settings_id) REFERENCES ticket_settings (id),
+    FOREIGN KEY (executor_id) REFERENCES users (id)
+);
+create TABLE IF NOT EXISTS ticket_settings_observers
+(
+    out_id             varchar(128),
+    deleted            bool NOT NULL DEFAULT 'false',
+    version            int4 NOT NULL DEFAULT (0),
+    id                 uuid NOT NULL,
+    ticket_settings_id uuid NOT NULL,
+    observer_id        uuid NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (ticket_settings_id) REFERENCES ticket_settings (id),
+    FOREIGN KEY (observer_id) REFERENCES users (id)
 );
