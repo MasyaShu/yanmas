f89d39a9657c87d2c58ba11516b143c51e0bd9a5
mShu <masyashu@gmail.com>
2020-12-17 14:40:41 +0500

    refactor Role

---
+++ AccountsAndUsers/src/test/java/ru/itterminal/botdesk/aau/service/impl/RoleServiceImplTest.java
@@ -0,0 +1,46 @@
+package ru.itterminal.botdesk.aau.service.impl;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import ru.itterminal.botdesk.aau.model.Role;
+import ru.itterminal.botdesk.aau.model.test.RoleTestHelper;
+import ru.itterminal.botdesk.aau.repository.RoleRepository;
+import ru.itterminal.botdesk.aau.service.validator.RoleOperationValidator;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SpringJUnitConfig(value = {RoleServiceImpl.class})
+@ActiveProfiles("Test")
+class RoleServiceImplTest {
+
+    private final RoleTestHelper roleTestHelper = new RoleTestHelper();
+
+    @MockBean
+    private RoleRepository repository;
+
+    @MockBean
+    private RoleOperationValidator validator;
+
+    @Autowired
+    private RoleServiceImpl service;
+
+    private final Role role = roleTestHelper.getPredefinedValidEntityList().get(0);
+
+
+    @Test
+    void update_shouldUpdateUser_whenPassedValidData() {
+        when(repository.getByName(any())).thenReturn(Optional.of(role));
+        Role findRole = service.getAccountOwnerRole();
+        assertEquals(findRole, role);
+        verify(repository, times(1)).getByName(any());
+    }
+}
