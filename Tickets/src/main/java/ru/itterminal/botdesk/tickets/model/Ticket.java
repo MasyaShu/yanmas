53ce8b7e6cef3b61aee96dba13f2cf3a6053c7e7
yaneg-ru <yaneg.ru@gmail.com>
2020-12-24 16:41:47 +0500

    start implement Ticket

---
+++ Tickets/src/main/java/ru/itterminal/botdesk/tickets/model/Ticket.java
@@ -0,0 +1,107 @@
+package ru.itterminal.botdesk.tickets.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import ru.itterminal.botdesk.aau.model.Account;
+import ru.itterminal.botdesk.aau.model.Group;
+import ru.itterminal.botdesk.aau.model.User;
+import ru.itterminal.botdesk.commons.model.BaseEntity;
+
+@Entity
+@Table(name = "ticket")
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class Ticket extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "account_id", nullable = false)
+    private Account account;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    @Column(nullable = false)
+    private Integer number;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Long createdAt;
+
+    @Column
+    private String subject;
+
+    @Column
+    private String description;
+
+    @Column
+    private Long deadline;
+
+    @Column(name = "is_finished", nullable = false, columnDefinition = "BOOLEAN DEFAULT FALSE")
+    private Boolean isFinished;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ticket_type_id")
+    private TicketType ticketType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ticket_status_id")
+    private TicketStatus ticketStatus;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ticket_template_id")
+    private TicketTemplate ticketTemplate;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ticket_inheritor_id")
+    private Ticket ticketInheritor;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "ticket_observers",
+            joinColumns = @JoinColumn(name = "ticket_id"),
+            inverseJoinColumns = @JoinColumn(name = "observer_id")
+    )
+    List<User> observers = new ArrayList<>();
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "ticket_executors",
+            joinColumns = @JoinColumn(name = "ticket_id"),
+            inverseJoinColumns = @JoinColumn(name = "executor_id")
+    )
+    List<User> executors = new ArrayList<>();
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = System.currentTimeMillis();
+    }
+
+    @Override
+    public void generateDisplayName() {
+        setDisplayName(number.toString());
+    }
+}
