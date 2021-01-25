564e6a46297167a0b43af68b2af20a6d73631804
yaneg-ru <yaneg.ru@gmail.com>
2021-01-25 08:01:25 +0500

    refactoring SortFieldsValidator

--- Commons/src/main/java/ru/itterminal/botdesk/commons/model/validator/sortfields/SortFieldsValidator.java
+++ Commons/src/main/java/ru/itterminal/botdesk/commons/model/validator/sortfields/SortFieldsValidator.java
@@ -7,20 +7,20 @@ import java.util.stream.Collectors;
 
 public class SortFieldsValidator implements ConstraintValidator<ValidateSortFields, List<String>> {
 
-    private String sortFields;
+    private String permittedFieldsForSort;
     private String message;
 
 
     @Override
     public void initialize(ValidateSortFields constraintAnnotation) {
-        sortFields = constraintAnnotation.permittedFieldsForSort();
+        permittedFieldsForSort = constraintAnnotation.permittedFieldsForSort();
         message = constraintAnnotation.message();
     }
 
     @Override
     public boolean isValid(List<String> value, ConstraintValidatorContext context) {
 
-        List<String> sortFieldsList = Arrays.asList(sortFields.toLowerCase().split(", "));
+        List<String> sortFieldsList = Arrays.asList(permittedFieldsForSort.toLowerCase().split(", "));
 
         String notValidField =
                 value.stream()
@@ -28,7 +28,9 @@ public class SortFieldsValidator implements ConstraintValidator<ValidateSortFiel
                         .collect(Collectors.joining(", "));
 
         if (!notValidField.isEmpty()) {
-            message = String.format("the passed values '%s' do not match the available sort values: %s", notValidField, sortFields);
+            message = String.format("the passed values '%s' do not match the available sort values: %s", notValidField,
+                                    permittedFieldsForSort
+            );
             context.disableDefaultConstraintViolation();
             context.buildConstraintViolationWithTemplate(message)
                     .addConstraintViolation();
