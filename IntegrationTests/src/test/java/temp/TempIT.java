c43225702bc6006228b881fdb1e5512616997b38
yaneg-ru <yaneg.ru@gmail.com>
2021-02-10 15:46:14 +0500

    rewrite pom in module IntegrationTests

--- IntegrationTests/src/test/java/temp/TempIT.java
+++ IntegrationTests/src/test/java/temp/TempIT.java
@@ -1,9 +1,11 @@
 package temp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.Profile;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class TempTest {
+class TempIT {
 
     @Test
     void method_should_when () {
