e7e39a4b5455d386ca7dd7a9e3a5cb6a4450591b
mShu <masyashu@gmail.com>
2021-02-11 10:25:14 +0500

    add upperCase for password in UserTestHelper; add PrePersist onCreate for Account, Group and User; Transactional from method in CrudServiceImpl; refactor AccountServiceImpl; add findByEmailForCreateAccount in UserServiceImpl; rename TempTest -> TempIT

---
+++ IntegrationTests/src/test/java/temp/TempIT.java
@@ -0,0 +1,14 @@
+package temp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.Profile;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TempIT {
+
+    @Test
+    void method_should_when () {
+        assertTrue(true);
+    }
+}
