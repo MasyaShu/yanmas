71af35c1306a41aaec0ea73083dc947947823b87
mShu <masyashu@gmail.com>
2021-04-11 13:19:01 +0500

    remove findByUniqueFields (service) in checkUniqueness (validator) and test

--- AccountsAndUsers/src/main/java/ru/itterminal/yanmas/aau/service/validator/GroupOperationValidator.java
+++ AccountsAndUsers/src/main/java/ru/itterminal/yanmas/aau/service/validator/GroupOperationValidator.java
@@ -1,33 +1,34 @@
 package ru.itterminal.yanmas.aau.service.validator;
 
-import static java.lang.String.format;
-import static ru.itterminal.yanmas.commons.util.CommonMethodsForValidation.checkStringForEquals;
-
-import java.util.List;
-
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import ru.itterminal.yanmas.aau.model.Group;
 import ru.itterminal.yanmas.aau.model.Roles;
-import ru.itterminal.yanmas.aau.model.projection.GroupUniqueFields;
+import ru.itterminal.yanmas.aau.repository.GroupRepository;
 import ru.itterminal.yanmas.aau.service.impl.GroupServiceImpl;
 import ru.itterminal.yanmas.commons.service.validator.impl.BasicOperationValidatorImpl;
 import ru.itterminal.yanmas.security.jwt.JwtUser;
+
+import static java.lang.String.format;
+import static ru.itterminal.yanmas.commons.util.CommonMethodsForValidation.checkStringForEquals;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class GroupOperationValidator extends BasicOperationValidatorImpl<Group> {
 
+    private final GroupServiceImpl service;
+    private final GroupRepository repository;
+
     public static final String ACCESS_IS_DENIED_FOR_SEARCHING_BY_PASSED_GROUP_ID =
             "Access is denied for searching by passed groupId";
-    private final GroupServiceImpl service;
-    private static final String USER_FROM_AN_INNER_GROUP_CANNOT_CREATE_UPDATE_GROUPS =
+        private static final String USER_FROM_AN_INNER_GROUP_CANNOT_CREATE_UPDATE_GROUPS =
             "A user from not inner group cannot create or update groups";
+    private static final String START_FIND_GROUP_BY_UNIQUE_FIELDS =
+            "Start find user by unique fields, name: {} and not id: {} and not account: {} and not isInner: {}";
 
     @Override
     public boolean logicalValidationBeforeUpdate(Group entity) {
@@ -39,7 +40,13 @@ public class GroupOperationValidator extends BasicOperationValidatorImpl<Group> 
     @Override
     public boolean checkUniqueness(Group entity) {
         log.trace(CHECK_UNIQUENESS, entity);
-        List<GroupUniqueFields> foundGroup = service.findByUniqueFields(entity);
+        log.trace(
+                START_FIND_GROUP_BY_UNIQUE_FIELDS, entity.getName(), entity.getId(), entity.getAccount(),
+                entity.getIsInner()
+        );
+        var foundGroup = repository.getByNameAndIsInnerAndAccount_IdAndIdNot(entity.getName(), entity.getIsInner(),
+                entity.getAccount().getId(), entity.getId()
+        );
         if (!foundGroup.isEmpty()) {
             String validatedField = "name";
             checkStringForEquals(entity.getName(), foundGroup.get(0).getName(),
