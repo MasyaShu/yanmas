594c1dd76ef60a6c513ade6a522f5908f16db770
mShu <masyashu@gmail.com>
2021-05-17 14:45:12 +0500

    new validators for Ticket check access before create and update;

---
+++ Tickets/src/main/java/ru/itterminal/yanmas/tickets/service/validator/ticket/check_access_before_create_update/CurrentUserRoleAdminOuterGroupCanNotCreateUpdateTicketIfTicketIsFromAnotherGroupValidator.java
@@ -0,0 +1,39 @@
+package ru.itterminal.yanmas.tickets.service.validator.ticket.check_access_before_create_update;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.stereotype.Component;
+import ru.itterminal.yanmas.aau.model.Roles;
+import ru.itterminal.yanmas.aau.model.User;
+import ru.itterminal.yanmas.aau.service.validator.EntityValidator;
+import ru.itterminal.yanmas.tickets.model.Ticket;
+
+@Component
+@RequiredArgsConstructor
+public class CurrentUserRoleAdminOuterGroupCanNotCreateUpdateTicketIfTicketIsFromAnotherGroupValidator implements EntityValidator<Ticket> {
+    public static final String
+            CURRENT_USER_WITH_ROLE_ADMIN_FROM_OUTER_GROUP_CAN_NOT_CREATE_UPDATE_TICKET_IF_TICKET_IS_FROM_ANOTHER_GROUP =
+            "Current user with role ADMIN from outer group can not create/update ticket if ticket is from another group";
+
+    @Override
+    public void checkAccessBeforeCreate(Ticket entity, User currentUser) {
+        checkAccessForCreateAndUpdate(entity, currentUser);
+    }
+
+    @Override
+    public void checkAccessBeforeUpdate(Ticket entity, User currentUser) {
+        checkAccessForCreateAndUpdate(entity, currentUser);
+    }
+
+    private void checkAccessForCreateAndUpdate(Ticket entity, User currentUser) {
+        if (currentUser.getRole().getName().equals(Roles.ADMIN.toString())
+                && Boolean.FALSE.equals(currentUser.getGroup().getIsInner())
+                && Boolean.FALSE.equals(entity.getAuthor().getGroup().getIsInner())
+                && !currentUser.getGroup().equals(entity.getAuthor().getGroup())) {
+            throw new AccessDeniedException
+                    (CURRENT_USER_WITH_ROLE_ADMIN_FROM_OUTER_GROUP_CAN_NOT_CREATE_UPDATE_TICKET_IF_TICKET_IS_FROM_ANOTHER_GROUP);
+        }
+
+
+    }
+}
