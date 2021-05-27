40bcc0ab0290a70e47ac179a1553090e96c5ff2e
mShu <masyashu@gmail.com>
2021-05-27 11:49:55 +0500

    fix TicketSettingOperationValidator

--- Tickets/src/main/java/ru/itterminal/yanmas/tickets/service/validator/ticket_setting/TicketSettingOperationValidator.java
+++ Tickets/src/main/java/ru/itterminal/yanmas/tickets/service/validator/ticket_setting/TicketSettingOperationValidator.java
@@ -12,9 +12,9 @@ public class TicketSettingOperationValidator extends BasicOperationValidatorImpl
     public static final String CURRENT_USER_FROM_OUTER_GROUP_CANNOT_GET_SETTING_OR_PREDEFINED_VALUES_FOR_TICKET_IF_HIS_GROUP_IS_NOT_EQUAL_GROUP_OF_AUTHOR_FROM_REQUEST =
             "Current user from outer group cannot get setting or predefined values for ticket if his group is not equal group of author from request";
 
-    public void checkAccessForGetSettingOrPredefinedValuesForTicket(User currentUSer, User foundUser) {
-        if (Boolean.FALSE.equals(foundUser.getGroup().getIsInner()) &&
-                !currentUSer.getGroup().getId().equals(foundUser.getGroup().getId())) {
+    public void checkAccessForGetSettingOrPredefinedValuesForTicket(User currentUser, User foundUser) {
+        if (Boolean.FALSE.equals(currentUser.getGroup().getIsInner()) &&
+                !currentUser.getGroup().getId().equals(foundUser.getGroup().getId())) {
             throw new AccessDeniedException(CURRENT_USER_FROM_OUTER_GROUP_CANNOT_GET_SETTING_OR_PREDEFINED_VALUES_FOR_TICKET_IF_HIS_GROUP_IS_NOT_EQUAL_GROUP_OF_AUTHOR_FROM_REQUEST);
         }
     }
