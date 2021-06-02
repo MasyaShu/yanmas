ce790228570efa90116617f455320c00f05cb409
yaneg-ru <fortune19171917>
2021-06-02 12:02:32 +0500

    continue implement TicketsFilesIT

--- IntegrationTests/src/test/java/ru/itterminal/yanmas/IT/Files/TicketsFilesIT.java
+++ IntegrationTests/src/test/java/ru/itterminal/yanmas/IT/Files/TicketsFilesIT.java
@@ -1,32 +1,8 @@
 package ru.itterminal.yanmas.IT.Files;
 
-import static io.restassured.RestAssured.given;
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static ru.itterminal.yanmas.IT.util.ITHelper.ACCOUNT_OWNER;
-import static ru.itterminal.yanmas.IT.util.ITHelper.ADMIN;
-import static ru.itterminal.yanmas.IT.util.ITHelper.APPLICATION_JSON;
-import static ru.itterminal.yanmas.IT.util.ITHelper.AUTHOR;
-import static ru.itterminal.yanmas.IT.util.ITHelper.EXECUTOR;
-import static ru.itterminal.yanmas.IT.util.ITHelper.FILE;
-import static ru.itterminal.yanmas.IT.util.ITHelper.FILE_DATA;
-import static ru.itterminal.yanmas.IT.util.ITHelper.MULTIPART;
-import static ru.itterminal.yanmas.IT.util.ITHelper.OBSERVER;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-import java.util.stream.Stream;
-
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestMethodOrder;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -36,32 +12,56 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
-
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import ru.itterminal.yanmas.IT.util.ITHelper;
 import ru.itterminal.yanmas.IT.util.ITTestConfig;
 import ru.itterminal.yanmas.aau.model.User;
 import ru.itterminal.yanmas.aau.repository.UserRepository;
 import ru.itterminal.yanmas.commons.exception.error.ApiError;
 import ru.itterminal.yanmas.files.model.dto.FileDto;
+import ru.itterminal.yanmas.files.repository.FileRepository;
 import ru.itterminal.yanmas.security.jwt.JwtProvider;
+import ru.itterminal.yanmas.tickets.model.Ticket;
+import ru.itterminal.yanmas.tickets.model.dto.TicketDtoResponse;
+import ru.itterminal.yanmas.tickets.model.test.TicketTestHelper;
+import ru.itterminal.yanmas.tickets.repository.TicketRepository;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.stream.Stream;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.*;
+import static ru.itterminal.yanmas.IT.util.ITHelper.*;
 
 @DataJpaTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@ContextConfiguration(classes = {ITTestConfig.class, JwtProvider.class, UserRepository.class})
+@ContextConfiguration(classes = {ITTestConfig.class, JwtProvider.class, UserRepository.class, FileRepository.class, TicketRepository.class})
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @TestPropertySource(properties = {"jwt.token.secret=ksedtob", "jwt.token.expired=8640000", "jwt.token.prefix=Bearer"})
 class TicketsFilesIT {
 
     @Autowired
+    private FileRepository fileRepository;
+
+    @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private TicketRepository ticketRepository;
+
 
     private static final ITHelper itHelper = new ITHelper();
     private final Map<User, FileDto> initialFiles = new HashMap<>();
     @SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
     private final Map<User, byte[]> initialFilesData = new HashMap<>();
+    private final TicketTestHelper ticketTestHelper = new TicketTestHelper();
+    private final Map<User, TicketDtoResponse> updatedTicket = new HashMap<>();
 
     byte[] biggestFileData = new byte[27000000];
 
@@ -107,8 +107,7 @@ class TicketsFilesIT {
 
     @Test
     @Order(20)
-    void LogicalExceptionWhenPutDataBecauseSizeOfFileMustNotBeMoreThan25MBByAllInitialUsers()
-    {
+    void LogicalExceptionWhenPutDataBecauseSizeOfFileMustNotBeMoreThan25MBByAllInitialUsers() {
         var accountOwner = itHelper.getAccountOwner();
         var fileCreatedByCurrentUser = initialFiles.get(accountOwner);
         var apiError = given().
@@ -135,8 +134,7 @@ class TicketsFilesIT {
 
     @Test
     @Order(30)
-    void LogicalExceptionBecauseFileMustBeUploadedBeforeGetData()
-    {
+    void LogicalExceptionBecauseFileMustBeUploadedBeforeGetData() {
         var accountOwner = itHelper.getAccountOwner();
         var fileCreatedByAccountOwner = initialFiles.get(accountOwner);
         var apiError = given().
@@ -193,7 +191,7 @@ class TicketsFilesIT {
         new Random().nextBytes(fileData);
         var accountOwner = itHelper.getAccountOwner();
         var fileCreatedByAccountOwner = initialFiles.get(accountOwner);
-         var apiError = given().
+        var apiError = given().
                 when().
                 headers(
                         "Authorization",
@@ -220,8 +218,7 @@ class TicketsFilesIT {
     @ParameterizedTest(name = "{index} User: {0}")
     @MethodSource("getStreamAllInitialUsers")
     @Order(60)
-    void SuccessGetDataByAllInitialUsers(String userKey, User currentUser)
-    {
+    void SuccessGetDataByAllInitialUsers(String userKey, User currentUser) {
         var fileCreatedByCurrentUser = initialFiles.get(currentUser);
         var actualFileData = given().
                 when().
@@ -240,16 +237,127 @@ class TicketsFilesIT {
         assertArrayEquals(expectedFileData, actualFileData);
     }
 
+    @Test
+    @Order(70)
+    void BadRequestBeforeCreateFileBecauseNameOfFileMustLessThan256Characters() {
+        var expectedFileDto = FileDto.builder()
+                .fileName(itHelper.getFaker().lorem().characters(257))
+                .build();
+        given().
+                when().
+                headers(
+                        "Authorization",
+                        "Bearer " + itHelper.getTokens().get(itHelper.getAccountOwner().getEmail())
+                )
+                .contentType(APPLICATION_JSON)
+                .body(expectedFileDto)
+                .post(FILE)
+                .then()
+                .log().body()
+                .body(containsString("size must be between 0 and 256"))
+                .statusCode(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    @Order(80)
+    void BadRequestBeforeCreateFileBecauseNameOfFileMustNotBeBlank() {
+        var expectedFileDto = FileDto.builder()
+                .fileName("  ")
+                .build();
+        given().
+                when().
+                headers(
+                        "Authorization",
+                        "Bearer " + itHelper.getTokens().get(itHelper.getAccountOwner().getEmail())
+                )
+                .contentType(APPLICATION_JSON)
+                .body(expectedFileDto)
+                .post(FILE)
+                .then()
+                .log().body()
+                .body(containsString("must not be blank"))
+                .statusCode(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @SuppressWarnings({"unused", "OptionalGetWithoutIsPresent"})
+    @ParameterizedTest(name = "{index} User: {0}")
+    @MethodSource("getStreamAllUsersWithRolesAccountOwnerAdminExecutorAuthor")
+    @Order(90)
+    void SuccessCreateTicketWithFileAndAfterCheckThatFileHasLinkToCreatedTicket(String userKey, User currentUser) {
+        var fileCreatedByCurrentUser = initialFiles.get(currentUser);
+        var newTicket = Ticket.builder()
+                .author(currentUser)
+                .subject(TICKET)
+                .build();
+        var newTicketDtoRequest = ticketTestHelper.convertEntityToDtoRequest(newTicket, true);
+        newTicketDtoRequest.setFiles(List.of(fileCreatedByCurrentUser.getId()));
+        var createdTicket = given().
+                when()
+                .headers(
+                        "Authorization",
+                        "Bearer " + itHelper.getTokens().get(currentUser.getEmail())
+                )
+                .contentType(APPLICATION_JSON)
+                .body(newTicketDtoRequest)
+                .post(TICKET)
+                .then()
+                .log().body()
+                .statusCode(HttpStatus.CREATED.value())
+                .extract().response().as(TicketDtoResponse.class);
+        var updatedFileAfterCreateTicket = fileRepository.findByIdAndAccountId(
+                fileCreatedByCurrentUser.getId(),
+                currentUser.getAccount().getId()
+        ).get();
+        assertEquals(updatedFileAfterCreateTicket.getEntityId(), createdTicket.getId());
+        updatedTicket.put(currentUser, createdTicket);
+    }
+
+    @SuppressWarnings({"unused", "OptionalGetWithoutIsPresent"})
+    @ParameterizedTest(name = "{index} User: {0}")
+    @MethodSource("getStreamAllUsersWithRolesAccountOwnerAdminExecutorAuthor")
+    @Order(100)
+    void SuccessUpdateTicketWithFileAndAfterCheckThatFileHasNotChanged(String userKey, User currentUser) {
+        var ticketDtoResponseCreatedByCurrentUser = updatedTicket.get(currentUser);
+        var fileBeforeUpdateTicket = fileRepository.findByIdAndAccountId(
+                ticketDtoResponseCreatedByCurrentUser.getId(),
+                currentUser.getAccount().getId()
+        ).get();
+        var ticketForUpdate = ticketRepository.findByIdAndAccountId(
+                ticketDtoResponseCreatedByCurrentUser.getId(),
+                currentUser.getAccount().getId()
+        ).get();
+        ticketForUpdate.setSubject(itHelper.getFaker().funnyName().name());
+        var ticketDtoRequestForUpdate = ticketTestHelper.convertEntityToDtoRequest(ticketForUpdate, false);
+        var updatedTicketDtoResponse = given().
+                when()
+                .headers(
+                        "Authorization",
+                        "Bearer " + itHelper.getTokens().get(currentUser.getEmail())
+                )
+                .contentType(APPLICATION_JSON)
+                .body(ticketDtoRequestForUpdate)
+                .put(TICKET)
+                .then()
+                .log().body()
+                .statusCode(HttpStatus.CREATED.value())
+                .extract().response().as(TicketDtoResponse.class);
+        assertThat(ticketDtoResponseCreatedByCurrentUser).usingRecursiveComparison()
+                .ignoringFields("version", "subject").isEqualTo(updatedTicketDtoResponse);
+    }
+
     @SuppressWarnings("unused")
     private static Stream<Arguments> getStreamAllInitialUsers() {
         var roles = itHelper.getRoleTestHelper().getRolesByNames(
-                List.of(
-                        ACCOUNT_OWNER,
-                        ADMIN,
-                        EXECUTOR,
-                        AUTHOR,
-                        OBSERVER
+                List.of(ACCOUNT_OWNER, ADMIN, EXECUTOR, AUTHOR, OBSERVER
                 )
+        );
+        return itHelper.getStreamUsers(roles, null);
+    }
+
+    @SuppressWarnings("unused")
+    private static Stream<Arguments> getStreamAllUsersWithRolesAccountOwnerAdminExecutorAuthor() {
+        var roles = itHelper.getRoleTestHelper().getRolesByNames(
+                List.of(AUTHOR, EXECUTOR, ADMIN, ACCOUNT_OWNER)
         );
         return itHelper.getStreamUsers(roles, null);
     }
