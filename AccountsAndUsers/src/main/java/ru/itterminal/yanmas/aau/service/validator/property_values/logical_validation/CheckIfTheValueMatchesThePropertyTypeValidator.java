cbdf706d3e5310de1dc46ee4cd8c471b5bb4c59d
masyashu@gmail.com <Ijjt5prvmg))&>
2021-06-24 21:00:10 +0500

    fix CheckIfTheValueMatchesThePropertyTypeValidator and test

--- AccountsAndUsers/src/main/java/ru/itterminal/yanmas/aau/service/validator/property_values/logical_validation/CheckIfTheValueMatchesThePropertyTypeValidator.java
+++ AccountsAndUsers/src/main/java/ru/itterminal/yanmas/aau/service/validator/property_values/logical_validation/CheckIfTheValueMatchesThePropertyTypeValidator.java
@@ -45,15 +45,14 @@ public class CheckIfTheValueMatchesThePropertyTypeValidator implements EntityVal
             }
         }
 
-        if (entity.getProperty().getTypeProperty().equals("boolean")) {
-            if (!entity.getValue().equalsIgnoreCase("true")
-                    && !entity.getValue().equalsIgnoreCase("false")) {
-                addValidationErrorIntoErrors(
-                        ERROR_VALUE,
-                        VALUE_IS_NOT_A_BOOLEAN,
-                        errors
-                );
-            }
+        if (entity.getProperty().getTypeProperty().equals("boolean")
+                && !entity.getValue().equalsIgnoreCase("true")
+                && !entity.getValue().equalsIgnoreCase("false")) {
+            addValidationErrorIntoErrors(
+                    ERROR_VALUE,
+                    VALUE_IS_NOT_A_BOOLEAN,
+                    errors
+            );
         }
 //TODO заменить на enum number and boolean
     }
--- AccountsAndUsers/src/test/java/ru/itterminal/yanmas/aau/service/validator/property_values/logical_validation/CheckIfTheValueMatchesThePropertyTypeValidatorTest.java
+++ AccountsAndUsers/src/test/java/ru/itterminal/yanmas/aau/service/validator/property_values/logical_validation/CheckIfTheValueMatchesThePropertyTypeValidatorTest.java
@@ -16,6 +16,7 @@ import static ru.itterminal.yanmas.commons.util.CommonConstants.SPRING_ACTIVE_PR
 import static ru.itterminal.yanmas.commons.util.CommonMethodsForValidation.createMapForLogicalErrors;
 
 
+@SuppressWarnings({"unused"})
 @SpringJUnitConfig(value = {CheckIfTheValueMatchesThePropertyTypeValidator.class})
 @ActiveProfiles(SPRING_ACTIVE_PROFILE_FOR_UNIT_TESTS)
 class CheckIfTheValueMatchesThePropertyTypeValidatorTest {
@@ -52,6 +53,36 @@ class CheckIfTheValueMatchesThePropertyTypeValidatorTest {
         assertEquals(0, errors.size());
     }
 
+    @ParameterizedTest(name = "{index} value: {0}")
+    @MethodSource("getNotValidValuesFromNumber")
+    void errorCheckingAStringForNumberType_whenValueNullOrEmpty(String value) {
+        var errors = createMapForLogicalErrors();
+        var property = Property.builder()
+                .typeProperty("number")
+                .build();
+        var propertyValue = PropertyValues.builder()
+                .property(property)
+                .value(value)
+                .build();
+        checkIfTheValueMatchesThePropertyTypeValidator.logicalValidationBeforeCreate(propertyValue, errors);
+        assertEquals(1, errors.size());
+    }
+
+    @ParameterizedTest(name = "{index} value: {0}")
+    @MethodSource("getNotValidValuesFromBoolean")
+    void errorCheckingAStringForBooleanType_whenValueNullOrEmpty(String value) {
+        var errors = createMapForLogicalErrors();
+        var property = Property.builder()
+                .typeProperty("boolean")
+                .build();
+        var propertyValue = PropertyValues.builder()
+                .property(property)
+                .value(value)
+                .build();
+        checkIfTheValueMatchesThePropertyTypeValidator.logicalValidationBeforeCreate(propertyValue, errors);
+        assertEquals(1, errors.size());
+    }
+
     private static Stream<Arguments> getValuesFromBoolean() {
         return Stream.of(
                 Arguments.of("true"),
@@ -64,16 +95,45 @@ class CheckIfTheValueMatchesThePropertyTypeValidatorTest {
     private static Stream<Arguments> getValuesFromNumber() {
         return Stream.of(
                 Arguments.of("1"),
-                Arguments.of("1,2"),
                 Arguments.of("1.1"),
                 Arguments.of("1"),
-                Arguments.of("1,r"),
-                Arguments.of("1.w"),
                 Arguments.of(".0"),
-                Arguments.of(".0."),
-                Arguments.of("0.")
+                Arguments.of("0."),
+                Arguments.of(Integer.toString(Integer.MAX_VALUE)),
+                Arguments.of(Integer.toString(Integer.MIN_VALUE)),
+                Arguments.of(Double.toString(Double.MAX_VALUE)),
+                Arguments.of(Double.toString(Double.MIN_VALUE)),
+                Arguments.of(Float.toString(Float.MAX_VALUE)),
+                Arguments.of(Float.toString(Float.MIN_VALUE)),
+                Arguments.of(Long.toString(Long.MAX_VALUE)),
+                Arguments.of(Long.toString(Long.MIN_VALUE))
         );
     }
 
+    private static Stream<Arguments> getNotValidValuesFromNumber() {
+        return Stream.of(
+                Arguments.of("1/"),
+                Arguments.of("1,1"),
+                Arguments.of(",1"),
+                Arguments.of(".0."),
+                Arguments.of(",0."),
+                Arguments.of("")
+        );
+    }
 
+    private static Stream<Arguments> getNotValidValuesFromBoolean() {
+        return Stream.of(
+                Arguments.of("1"),
+                Arguments.of("0"),
+                Arguments.of("Y"),
+                Arguments.of("N"),
+                Arguments.of("yes"),
+                Arguments.of("no"),
+                Arguments.of("trues"),
+                Arguments.of("is false"),
+                Arguments.of("plus"),
+                Arguments.of("")
+        );
+    }
+//TODO заменить на enum number and boolean
 }
