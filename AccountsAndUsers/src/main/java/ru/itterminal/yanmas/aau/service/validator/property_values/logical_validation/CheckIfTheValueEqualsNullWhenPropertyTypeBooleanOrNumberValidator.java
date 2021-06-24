955c52d26c56284de8e875efc966ca1760c3ea0e
masyashu@gmail.com <Ijjt5prvmg))&>
2021-06-24 21:34:48 +0500

    add CheckIfTheValueEqualsNullWhenPropertyTypeBooleanOrNumberValidator

---
+++ AccountsAndUsers/src/main/java/ru/itterminal/yanmas/aau/service/validator/property_values/logical_validation/CheckIfTheValueEqualsNullWhenPropertyTypeBooleanOrNumberValidator.java
@@ -0,0 +1,39 @@
+package ru.itterminal.yanmas.aau.service.validator.property_values.logical_validation;
+
+import ru.itterminal.yanmas.aau.model.PropertyValues;
+import ru.itterminal.yanmas.aau.service.validator.EntityValidator;
+import ru.itterminal.yanmas.commons.exception.error.ValidationError;
+
+import java.util.List;
+import java.util.Map;
+
+import static ru.itterminal.yanmas.commons.util.CommonConstants.MUST_NOT_BE_NULL;
+import static ru.itterminal.yanmas.commons.util.CommonMethodsForValidation.addValidationErrorIntoErrors;
+
+public class CheckIfTheValueEqualsNullWhenPropertyTypeBooleanOrNumberValidator implements EntityValidator<PropertyValues> {
+
+    public static final String ERROR_VALUE = "error value";
+
+    @Override
+    public void logicalValidationBeforeUpdate(PropertyValues entity, Map<String, List<ValidationError>> errors) {
+        checkIfTheValueMatchesThePropertyType(entity, errors);
+    }
+
+    @Override
+    public void logicalValidationBeforeCreate(PropertyValues entity, Map<String, List<ValidationError>> errors) {
+        checkIfTheValueMatchesThePropertyType(entity, errors);
+    }
+
+    private void checkIfTheValueMatchesThePropertyType(PropertyValues entity, Map<String, List<ValidationError>> errors) {
+        if ((entity.getProperty().getTypeProperty().equals("number")
+                || entity.getProperty().getTypeProperty().equals("boolean"))
+                && entity.getValue() == null) {
+            addValidationErrorIntoErrors(
+                    ERROR_VALUE,
+                    MUST_NOT_BE_NULL,
+                    errors
+            );
+        }
+//TODO заменить на enum number and boolean
+    }
+}
