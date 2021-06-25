d61efd5ab8131d4e97859befaab31e5f2208eeea
yaneg-ru <fortune19171917>
2021-06-25 10:12:14 +0500

    add enum TypeEntity add enum TypeProperty

---
+++ AccountsAndUsers/src/main/java/ru/itterminal/yanmas/aau/model/TypeEntity.java
@@ -0,0 +1,10 @@
+package ru.itterminal.yanmas.aau.model;
+
+@SuppressWarnings("unused")
+public enum TypeEntity {
+    ACCOUNT,
+    USER,
+    GROUP,
+    TICKET,
+    TICKET_EVENT
+}
---
+++ AccountsAndUsers/src/main/java/ru/itterminal/yanmas/aau/model/TypeProperty.java
@@ -0,0 +1,10 @@
+package ru.itterminal.yanmas.aau.model;
+
+@SuppressWarnings("unused")
+public enum TypeProperty {
+    ACCOUNT,
+    USER,
+    GROUP,
+    TICKET,
+    TICKET_EVENT
+}
